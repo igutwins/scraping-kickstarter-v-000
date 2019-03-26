9403e6b8e26fa302f05e364e12f292fc08da5d9c
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-26 02:00:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,8 +9,11 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css("li.project.grid_4")
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
 
+  projects 
 binding.pry
 end
 
